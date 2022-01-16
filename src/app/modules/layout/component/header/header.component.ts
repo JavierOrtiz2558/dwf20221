9b812c6ee1dc4a911e4353911c3f75082ab4676a
Javier <w.jav56@ciencias.unam.mx>
2022-01-16 05:18:29 -0600

    Se agrega HU productos aleatorios, Responsable Javier Ortiz

--- src/app/modules/layout/component/header/header.component.ts
+++ src/app/modules/layout/component/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/modules/product/_model/category'; 
+import { CategoryService } from 'src/app/modules/product/_service/category.service'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  //categorias asociadas al producto
+  categories: Category[] = [];
+  category: Category = new Category();
+
+  constructor(
+    private cateory_service: CategoryService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(){
+    this.cateory_service.getCategories().subscribe(
+      res => {
+        this.categories = res;
+      },
+      err => console.log(err)
+    )
+  }
+
+  getCategory(id_category: number){
+    this.cateory_service.getCategory(id_category).subscribe(
+      res => {
+        this.category = res;
+      },
+      err => console.log(err)
+    )
   }
 
 }
